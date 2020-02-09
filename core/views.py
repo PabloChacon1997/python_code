eae5d0c8237c4d7cde4cb01dae7644c1849f2262
PabloChacon1997 <47465321+PabloChacon1997@users.noreply.github.com>
2020-02-09 18:33:20 -0500

    Final commit

--- core/views.py
+++ core/views.py
@@ -4,13 +4,28 @@ import base64
 import json
 from uuid import uuid4
 from django.views.decorators.csrf import csrf_exempt
+import pyrebase
+from datetime import date
 # Create your views here.
 
 
 @csrf_exempt
 def subirImagen(request):
+    firebaseConfig = {
+        'apiKey': "AIzaSyBRWvbJvMjs4J_hPUsTKaPQKt7-ISK3T2o",
+        'authDomain': "pythonfirebase-f55ac.firebaseapp.com",
+        'databaseURL': "https://pythonfirebase-f55ac.firebaseio.com",
+        'projectId': "pythonfirebase-f55ac",
+        'storageBucket': "pythonfirebase-f55ac.appspot.com",
+        'messagingSenderId': "1012266754514",
+        'appId': "1:1012266754514:web:b40da7bfc04cbcbf87280b"
+    }
+    firebase = pyrebase.initialize_app(firebaseConfig)
+
     if request.method == 'POST':
         img = request.FILES['imagen']
+        body = request.POST.get('usuario')
+        men = request.POST.get('mensaje')
         imagen_str = base64.b64encode(img.read()).decode('utf-8')
         ext = img.content_type.replace('image/', '.')
         print(imagen_str)
@@ -21,10 +36,29 @@ def subirImagen(request):
                 'nombre': f'{uuid4()}{ext}'
             })
         )
-        return JsonResponse(res.json())
+        hoy = date.today()
+        resjson = res.json()
+        res1 = json.dumps(resjson)
+        res3 = json.loads(res1)
+        urlbucket = res3['url']
+        print(urlbucket , hoy)
+        d1 = hoy.strftime("%d/%m/%Y")
+        firebaseRes = requests.post('https://pythonfirebase-f55ac.firebaseio.com/datos.json',
+        json=json.dumps({
+            'fecha': d1,
+            'mensaje': men,
+            'url': urlbucket,
+            'usuario': body
+            })
+        )
+        return JsonResponse(firebaseRes.json())
+    if request.method == 'GET':
+        res2 = requests.get('https://pythonfirebase-f55ac.firebaseio.com/datos.json')
+        return JsonResponse(res2.json())
+
 
     return JsonResponse({
         'status': 400,
-        'message': 'ERROR: Solo se aceptan peticiones POST'
+        'message': 'ERROR: Hacer un GET o POST'
     })
 
